e63472d868437dba8d9d65fa10947d20ecf05948
Manuel Villanueva <1mm.villanueva1@gmail.com>
2020-05-09 16:32:38 -0600

    addded configuration for automapper

---
+++ TecolocoTest/WebApp/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Helpers
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Jobs, JobsDTO>();
+            CreateMap<JobsDTO, Jobs>();
+        }
+    }
+}
