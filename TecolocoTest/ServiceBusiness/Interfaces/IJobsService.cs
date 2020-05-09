cec5147899bf1febdf97cf2140988e04ca99979c
Manuel Villanueva <1mm.villanueva1@gmail.com>
2020-05-09 14:00:05 -0600

    service class to manage the logic for the jobs

---
+++ TecolocoTest/ServiceBusiness/Interfaces/IJobsService.cs
@@ -0,0 +1,16 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServiceBusiness.Interfaces
+{
+    public interface IJobsService
+    {
+        Jobs GetJobById(int id);
+        Jobs AddJob(Jobs job);
+        Jobs UpdateJob(Jobs job);
+        void DeleteJob(int id);
+        List<Jobs> GetListJobs();
+    }
+}
---
+++ TecolocoTest/ServiceBusiness/Services/JobsService.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.Context;
+using DataAccess.Models;
+using ServiceBusiness.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceBusiness.Services
+{
+    public class JobsService : IJobsService
+    {
+        private readonly TecolocoDbContext _dbcontext;
+        public JobsService(TecolocoDbContext context)
+        {
+            _dbcontext = context;
+        }
+        public Jobs AddJob(Jobs job)
+        {
+            _dbcontext.Jobs.Add(job);
+            _dbcontext.SaveChanges();
+            return job;
+        }
+
+        public void DeleteJob(int id)
+        {
+            var auxJob = _dbcontext.Jobs.Find(id);
+            _dbcontext.Jobs.Remove(auxJob);
+            _dbcontext.SaveChanges();
+        }
+
+        public Jobs GetJobById(int id)
+        {
+            return _dbcontext.Jobs.Find(id);
+        }
+
+        public List<Jobs> GetListJobs()
+        {
+            return _dbcontext.Jobs.ToList();
+        }
+
+        public Jobs UpdateJob(Jobs job)
+        {
+            _dbcontext.Jobs.Update(job);
+            _dbcontext.SaveChanges();
+            return job;
+        }
+    }
+}
