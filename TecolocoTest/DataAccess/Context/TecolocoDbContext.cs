56f5d68efd338fcbc516ff33a503e36d86ba2407
Manuel Villanueva <1mm.villanueva1@gmail.com>
2020-05-09 01:55:08 -0600

    DataAccess Layer

---
+++ TecolocoTest/DataAccess/Context/TecolocoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Context
+{
+    public class TecolocoDbContext: DbContext
+    {
+
+        public TecolocoDbContext(DbContextOptions<TecolocoDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Jobs> Jobs { get; set; }
+ 
+    }
+}
---
+++ TecolocoTest/DataAccess/Models/Jobs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Models
+{
+   public class Jobs
+    {
+        public int Id { get; set; }
+        public string JobTitle { get; set; }
+        public string Descripcion { get; set; }
+        public System.DateTime CreatedAt { get; set; }
+        public System.DateTime ExpiredAt { get; set; }
+    }
+}
